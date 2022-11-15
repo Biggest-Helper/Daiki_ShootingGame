80f55ffc5c217384b67880235bcc6e2fa1086528
Biggest Helper <107020759+Biggest-Helper@users.noreply.github.com>
2022-11-15 13:12:48 +0900

    エネミー射撃処理実装途中

--- Daiki_ShootingGame/Daiki_ShootingGame/Enemy.cpp
+++ Daiki_ShootingGame/Daiki_ShootingGame/Enemy.cpp
@@ -1,5 +1,6 @@
 #include "DxLib.h"
 #include "Enemy.h"
+#include "StraightBullets.h"
 
 Enemy::Enemy(T_Location location) : CharaBase(location, 20.f, T_Location{0, 0.5}), hp(10), point(10)
 {
@@ -11,6 +12,42 @@ void Enemy::Updata()
 	T_Location newLocation = GetLocation();
 	newLocation.y += speed.y;
 	SetLocation(newLocation);
+
+	int bulletCount;
+	for (bulletCount = 0; bulletCount < 30; bulletCount++)
+	{
+		//弾切れがないかチェック
+		if (bullets[bulletCount] == nullptr)
+		{
+			break;
+		}
+		bullets[bulletCount]->Update();
+
+		//画面外に行ったら弾を消す
+		if (bullets[bulletCount]->isScreenOut())
+		{
+			delete bullets[bulletCount];
+			bullets[bulletCount] = nullptr;
+
+			/*配列を前に詰める(ソート)*/
+			for (int i = bulletCount; i < (30 - 1); i++)
+			{
+				if (bullets[i + 1] == nullptr)
+				{
+					break;
+				}
+				bullets[i] = bullets[i + 1];
+				bullets[i + 1] = nullptr;
+			}
+			bulletCount--;
+		}
+	}
+
+	//弾発射処理
+	if (bulletCount < 30 && bullets[bulletCount] == nullptr);
+	{
+		bullets[bulletCount] = new StraightBullets(GetLocation());
+	}
 }
 
 void Enemy::Draw()
